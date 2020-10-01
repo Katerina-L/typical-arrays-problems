5c9c54fd19c10f7805a23132cdea2e08a0aba741
Katerina-L <70204985+Katerina-L@users.noreply.github.com>
2020-10-01 15:31:43 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0)
+        {
+            return 0;
+        }
+    else {
+            let minitem = Math.min(...array);
+            return minitem;
+        }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0)
+    {
+        return 0;
+    }
+    else {
+        let maxitem = Math.max(...array);
+        return maxitem ;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sumarray =0;
+        if (array == undefined || array.length == 0)
+    {
+        return 0;
+    }
+    else {
+        for ( i=0; i<array.length; i++)
+            {
+                sumarray= sumarray + array[i];
+            }
+         let avgarray = sumarray/array.length;
+        return avgarray;
+    }
 }
